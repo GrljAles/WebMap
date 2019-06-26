22166e2dae5bc6150a746de7b37bc591438e999d
GrljAles <alesinar.grlj@gmail.com>
2019-06-26 21:50:36 +0200

    Implemented menu sliders functionallity and started working on the legend generator. Class breaks are being calculated on the fly with min/max slider. Also edited Ndvi legend colours array so I can start working on actual html generation for continous legend. Discrete legends demand slightly different approach and will need another function.

--- src/basemap.js
+++ src/basemap.js
@@ -1,3 +1,4 @@
+import { MdCollapsible } from "aurelia-materialize-bridge";
 import {inject} from 'aurelia-framework';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
@@ -21,6 +22,8 @@ export class BaseMap {
     this.userNameDisplay = null;
     this.opacityValue = 1;
     this.layers = dataData.default;
+    this.collapsible = MdCollapsible;
+    this.activeLayer = 0;
     this.subscribe();
   }
   setOpacity1(value) {
@@ -37,6 +40,7 @@ export class BaseMap {
   }
 
   attached() {
+    var _this = this
     for (var ii in this.layers) {
       var opacitySlider = document.getElementById('opacity-slider'+this.layers[ii].id);
         noUiSlider.create(opacitySlider, {
@@ -45,39 +49,51 @@ export class BaseMap {
         range: {
           'min': [0],
           'max': [1]
-        }
+        },
+        connect: 'lower'
       });
-      var adsa = document.getElementById('opacityValue'+this.layers[ii].id);
 
-      var _this = this
       opacitySlider.noUiSlider.on('update', function (values, handle) {
-        var opacity = values[handle]
-        adsa.innerHTML = values[handle]
-        function chop() {
-          _this.layers[ii].opacity = opacity
-        }
-        chop()
+        _this.layers[_this.activeLayer].opacity = values[handle]
+        _this.changeOpacity(_this.activeLayer, values[handle])
       });
-
-      //this.layers[ii].opacity = opacitySlider.opacity
 
       var dateSlider = document.getElementById('date-slider'+this.layers[ii].id);
         noUiSlider.create(dateSlider, {
-        start: [1],
+        start: [0],
+        step: 1,
         orientation: 'horizontal', // 'horizontal' or 'vertical'
         range: {
-          'min': [0],
-          'max': [1]
-        }
+          'min': [(this.layers[ii].availableDates.length - 1) * -1],
+          'max': [0]
+        },
+        connect: 'upper'
       });
+
+      dateSlider.noUiSlider.on('update', function (values, handle) {
+        let value = parseInt(values[handle])
+        _this.layers[_this.activeLayer].selectedDateIndex = value
+        _this.changDisplayedDate(_this.activeLayer, value)
+      });
+
       var minmaxSlider = document.getElementById('min-max-slider'+this.layers[ii].id);
         noUiSlider.create(minmaxSlider, {
-        start: [1],
+        start: [this.layers[ii].statistics.min, this.layers[ii].statistics.max],
         orientation: 'horizontal', // 'horizontal' or 'vertical'
         range: {
-          'min': [0],
-          'max': [1]
-        }
+          'min': [this.layers[ii].statistics.min],
+          'max': [this.layers[ii].statistics.max]
+        },
+        connect: true,
+        behaviour: 'drag-tap'
+      });
+
+      minmaxSlider.noUiSlider.on('update', function (values, handle) {
+        let valueMin = parseFloat(values[0]);
+        let valueMax = parseFloat(values[1]);
+        _this.layers[_this.activeLayer].displaySettings.min = valueMin
+        _this.layers[_this.activeLayer].displaySettings.max = valueMax
+        _this.calculateClassBreaks(_this.activeLayer, valueMin, valueMax)
       });
     }
 
@@ -121,27 +137,75 @@ export class BaseMap {
       }),
     });
   }
-  changDisplayedDate(e, id, dateIndex){
-    let displayedDate = this.layers[id].availableDates[dateIndex]
-    this.layers[id].displayedDate = moment(displayedDate, 'YYYYMMDD').format('DD.MM.YYYY')
-    this. changeLayerDate(e, id, displayedDate)
+  changDisplayedDate(idx, dateIndex){
+    this.activeLayer = this.layers[idx].id;
+    let displayedDate = this.layers[idx].availableDates[(this.layers[idx].availableDates.length -1) + dateIndex]
+    this.layers[idx].displayedDate = moment(displayedDate, 'YYYYMMDD').format('DD.MM.YYYY')
+    this.changeLayerDate(idx, displayedDate)
+    this.calculateClassBreaks(idx, this.layers[idx].displaySettings.min, this.layers[idx].displaySettings.max)
   }
-  changeOpacity(e, id, opacityValue) {
-    for (var ii of this.basemap.getLayers().getArray()) {
-      if (ii.getProperties().id === id) {
-        ii.setOpacity(opacityValue)
+  changeOpacity(id, opacityValue) {
+    if (this.basemap){
+      for (var ii of this.basemap.getLayers().getArray()) {
+        if (ii.getProperties().id === id) {
+          ii.setOpacity(opacityValue)
+        }
       }
     }
   }  
-  changeLayerDate(e, id, layerDate) {
-    for (var ii of this.basemap.getLayers().getArray()) {
-      if (ii.getProperties().id === id) {
-        let newSource = ii.getSource()
-        newSource.updateParams({
-            'date': layerDate
-          });
-        ii.setSource(newSource)
+  changeLayerDate(idx, layerDate) {
+      if (this.basemap){
+      for (var ii of this.basemap.getLayers().getArray()) {
+        if (ii.getProperties().id === idx) {
+          let newSource = ii.getSource()
+          newSource.updateParams({
+              'date': layerDate
+            });
+          ii.setSource(newSource)
+        }
       }
     }
   }
+  calculateClassBreaks(idx, min, max){
+    if (this.basemap){
+      let nClasses = this.layers[idx].displaySettings.nClasses;
+      let classRange = (max - min) / nClasses;
+      let classBreaks = [min];
+      for (let ii = 0; ii < nClasses; ii++) {
+        min += classRange;
+        classBreaks.push(min)
+      }
+      console.log(classBreaks)
+    }
+  }
+/*   calculateLegend(idx, classBreaks) {
+    let legenWraper = document.getElementById('legend' + idx);
+    if (legenWraper) {
+      legenWraper.parentNode.removeChild(legendContainer);
+    };
+    let breaks = this.productList[a].classBreaks[timeselection];
+    let colours = this.productList[a].classBreaks.colour;
+    let breaksLen = this.objectSize(breaks);
+    let colourBoxWidth = 100 / breaksLen;
+    let legend = document.createElement('div');
+    legend.id = 'legendados' + a;
+    legend.classList.add('legend-image');
+    for (let i = breaksLen; i >= 1; i--) {
+      let colourBox = document.createElement('div');
+      colourBox.classList.add('colour-box');
+      if (this.objectSize(colours['c' + i]) === 2) {
+        var colour1 = 'rgb(' + colours['c' + i].b1.r + ', ' + colours['c' + i].b1.g + ', ' + colours['c' + i].b1.b + ')';
+        let colour2 = 'rgb(' + colours['c' + i].b2.r + ', ' + colours['c' + i].b2.g + ', ' + colours['c' + i].b2.b + ')';
+        colourBox.setAttribute('style', 'background: linear-gradient(to right, ' + colour1 + ' 0%, ' + colour2 + ' 100%); width: ' + colourBoxWidth + '%');
+      } else {
+        var colour1 = 'rgb(' + colours['c' + i].b1.r + ', ' + colours['c' + i].b1.g + ', ' + colours['c' + i].b1.b + ')';
+        colourBox.setAttribute('style', 'background: ' + colour1 + '; width: ' + colourBoxWidth + '%');
+      }
+      colourBox.innerText = breaks['b' + i];
+      legend.appendChild(colourBox);
+    }
+    let lhId = 'legend-holder' + a;
+    document.getElementById(lhId).appendChild(legend);
+  } 
+  }*/
 }
